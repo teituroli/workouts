fe27a86d00b076550115a1fe0719bc911c5f9448
Teitur Óli Kristjánsson <teituroli@hotmail.com>
2026-04-27 10:35:14 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/gymfine/', // Change this to your GitHub repo name
+  base: '/workouts/', // Change this to your GitHub repo name
 })
